f7243ecf16ca9e706be7a2f04c75258e8b1e6803
Camey Fiifi Danchi <capochinolive100@gmail.com>
2024-12-02 22:21:43 +0000

    changes

--- main.js
+++ main.js
@@ -22,7 +22,17 @@ function moveSlide(direction) {
 // Automatically move to the next slide every 3 seconds
 setInterval(() => {
     moveSlide(1);
-}, 9500);
+}, 9000);
 
 // Initial slide
 showSlide(currentSlide);
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {  // Adjust the scroll position as needed
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
