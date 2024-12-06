c6bc8d42d00cc5f2c0fe6ec8b932c17ec21f3032
Camey Fiifi Danchi <capochinolive100@gmail.com>
2024-12-06 20:24:20 +0000

    changes

--- main.js
+++ main.js
@@ -36,3 +36,23 @@ window.addEventListener('scroll', function() {
     }
 });
 
+// Select elements
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links a');
+
+// Toggle the mobile menu
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('toggle');
+});
+
+// Close the menu when a link is clicked
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('toggle');
+    });
+});
+
+
